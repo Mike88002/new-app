63ae7f475b9aaf32c28ea845cd8d2fc9759c5fb0
Mike88002 <miky882002@gmail.com>
2026-03-14 21:29:54 +0100

    arrived at lesson 118 - viewed different ways to use hostbinding

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,12 +8,23 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()',
   }
 })
 export class ControlComponent {
+  // si puo usare l'host anche con il decoratore, optional input. ma non è consigliato per ragioni di compatibilità
+  // @HostBinding('class') className = 'control';
+
+  //tutte alternative ad usare  '(click)': 'onClick()' , ma l'angular team consiglia di mettere tutto in host all'interno di  @Component
+  // @HostListener('click') onCliCk() {
+  //   console.log('click')
+  // }
 
   label = input.required<string>();
-  
+
+  onClick() {
+    console.log('click')
+  }
 
 }
