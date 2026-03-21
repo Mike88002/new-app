9cc259e861c2faaae7c4e39c28693838a6b13f69
Mike88002 <miky882002@gmail.com>
2026-03-21 11:44:51 +0100

    arrived at lesson 124 - using setInterval in constructor

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -22,9 +22,11 @@ export class ControlComponent {
   // }
 
   label = input.required<string>();
+  private el = inject(ElementRef);
 
   onClick() {
     console.log('click')
+    console.log(this.el);
   }
 
 }
