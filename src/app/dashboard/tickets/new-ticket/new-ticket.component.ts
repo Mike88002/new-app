c5d33083dd1a94ec8e1216b51b6dd55b48b16ba6
Mike88002 <miky882002@gmail.com>
2026-03-28 22:47:47 +0100

    arrived at lesson 135 - difference between contentchild and viewchild

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
+
 export class NewTicketComponent {
+  // #titleInput => TEMPLATE VARIABLE
+
+  //ci aiuta a capire quali sono i child components nel componente. ha bisogno di un selector. posso passare
+  //anche template variables.
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+
+
+  // è possibile usare anche la viewchild function che ritorna un signal che lavora con qualsiasi type. usiamo () dal momento
+  // che è un segnale required garantisce che l'elemento venga trovato, quindi non c'è bisogno di usare la ? notation
+  private functionForm = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  
+  
+
+  onSubmit(titleElement: HTMLInputElement, textArea: string) {
+    const entered = titleElement.value;
+    console.dir(entered, textArea);
+
+    this.form?.nativeElement.reset();
+
+    this.functionForm().nativeElement.reset();
+  }
 
 }
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, contentChild, ContentChild, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -13,20 +13,32 @@ import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEn
   }
 })
 export class ControlComponent {
-  // si puo usare l'host anche con il decoratore, optional input. ma non è consigliato per ragioni di compatibilità
+  // si puo usare l'host anche con il decoratore, optional input.
+  //  ma non è consigliato per ragioni di compatibilità
   // @HostBinding('class') className = 'control';
 
-  //tutte alternative ad usare  '(click)': 'onClick()' , ma l'angular team consiglia di mettere tutto in host all'interno di  @Component
+  //tutte alternative ad usare  '(click)': 'onClick()' , ma l'angular team consiglia
+  //  di mettere tutto in host all'interno di  @Component
   // @HostListener('click') onCliCk() {
   //   console.log('click')
   // }
 
+  //il contenuto di ng-content, non è davvero parte del template, quindi viewchild e dren prendono solo quello
+  //che è parte del template. si usa contentChild o contentChildren
+
+  
   label = input.required<string>();
   private el = inject(ElementRef);
 
+  @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+
+  private controlFunction = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
   onClick() {
     console.log('click')
-    console.log(this.el);
+    //console.log(this.el);
+    console.log(this.control);
+    console.log(this.controlFunction());
   }
 
 }
