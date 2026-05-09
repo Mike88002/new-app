185a8bb8785a9361986c8e45b3cb0f2ae1763cec
Mike88002 <miky882002@gmail.com>
2026-05-09 10:08:12 +0200

    arrived at lesson 146

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,16 +1,27 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { Ticket } from './ticket.model';
 import { NewTicketComponent } from "../new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent,],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
 
   ticketData = input.required<Ticket>(); 
+  detailsVisible = signal(false);
+  closed = output();
+
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible());
+    this.detailsVisible.update((wasVisibile) => !wasVisibile );
+  }
+
+  onMarkAsCompleted() {
+    this.closed.emit();
+  }
 
 }
